42a94a90d267b4423c56c74035e73e3ffc3e918a
Sergey Naryshkin <snowserge@gmail.com>
2018-01-28 12:41:05 +0300

    Added DbConnection

--- sql2orm.php
+++ sql2orm.php
@@ -6,3 +6,11 @@
 require_once 'vendor/autoload.php';
 
 date_default_timezone_set("Europe/Moscow");
+$usage = "Usage: {$argv[0]} dbname dbuser password\n";
+if(count($argv) !== 4) {
+    echo $usage;
+} else {
+    echo "SQL {$argv[1]} {$argv[2]} {$argv[3]} \n";
+}
+//echo "Enter "
+//$db =readline();
---
+++ src/Db/DbConnection.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author: Sergey Naryshkin
+ * @date 28.01.2018
+ */
+
+namespace SnowSerge\Sql2Orm\Db;
+
+
+class DbConnection
+{
+    /** @var \PDO */
+    private $pdo;
+
+    /** @var string|null */
+    private $schema;
+    /**
+     * DbConnection constructor.
+     * @param string $host
+     * @param string $port
+     * @param string $user
+     * @param string $password
+     * @param string $nameDb
+     * @param string $driver
+     * @param string|null $schema
+     */
+    public function __construct($host, $port, $user, $password, $nameDb,$driver='mysql',$schema = null)
+    {
+        $dsn = $driver . ':' . 'host=' . $host . ';dbname='.$nameDb . ';port='. $port.';charset=UTF8';
+        $this->pdo = new \PDO($dsn,$user,$password);
+    }
+
+    /**
+     * @return \PDO
+     */
+    public function getPdo(): \PDO
+    {
+        return $this->pdo;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getSchema(): ?string
+    {
+        return $this->schema;
+    }
+
+
+
+}
